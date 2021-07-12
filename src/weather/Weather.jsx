eac5b3427ca32d58ea9c6ebba2670a2befbebb24
OZLEMCITCI <ozlmctc@gmail.com>
2021-07-12 08:28:00 -0500

    commit

--- src/weather/Weather.jsx
+++ src/weather/Weather.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const Weather = () => {
+const Weather = (probs) => {
     return (
         <div className="contanier">
             <div className="cards">
-                <h1>London</h1>
+                <h1>{probs.city},{probs.country }</h1>
                 <h5 className="py-4">
-                    <i class="wi wi-day-sunny display-1"></i>
+                    <i className={`wi ${probs.icon} display-1`}></i>
                 </h5>
                 <h1 className="py-3">25&deg;</h1>
                 {/* minmax funksiyonu */}
-                {minmaxTemp(23, 35)}
+                {minmaxTemp(probs.min, probs.max)}
                 
                 <h4 className="py-3">Slow Rain</h4>
             </div>
